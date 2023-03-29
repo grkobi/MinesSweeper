0cdfcfb93675610a7178497b3e5887720704dabb
Kobi Greidinger <kobi.greidinger@arm.com>
2023-03-29 16:01:52 +0300

    Add files via upload

---
+++ MinesSweeper/js/app.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var gBoard;
+var gLevel;
+var gIsGameOver = false;
+
+const MINE_IMG = '<img src="img/mine.png">'
+const FLAG_IMG = '<img src="img/flag.png">'
+
+var gGame = {
+    isOn: false,
+    shownCount: 0,
+    markedCount: 0,
+    secsPassed: 0
+}
+
+function buildBoard() {
+    gBoard = []
+    for (var i = 0 ; i<)
+}
+
+function setMinesNegsCount() {
+
+}
+
+function renderBoard() {
+
+}
